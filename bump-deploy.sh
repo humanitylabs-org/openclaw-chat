819ae1d16011ff0e59838d2aa88f5e22319e8ef8
Oscar Collins <oscarhenrycollins@gmail.com>
2026-04-01 23:24:16 +0000

    fix: remove dead sw.js from bump script

--- bump-deploy.sh
+++ bump-deploy.sh
@@ -2,9 +2,6 @@
 # Auto-bump all cache version strings and deploy
 set -e
 
-# Bump sw.js CACHE_VERSION
-perl -i -pe 's/const CACHE_VERSION = "v(\d+)"/sprintf("const CACHE_VERSION = \"v%d\"", $1+1)/e' sw.js
-
 # Bump app.js?v= in index.html
 perl -i -pe 's/app\.js\?v=(\d+)/sprintf("app.js?v=%d", $1+1)/e' index.html
 
@@ -12,7 +9,6 @@ perl -i -pe 's/app\.js\?v=(\d+)/sprintf("app.js?v=%d", $1+1)/e' index.html
 perl -i -pe 's/theme\.css\?v=(\d+)/sprintf("theme.css?v=%d", $1+1)/e' index.html
 
 echo "Versions bumped:"
-grep 'CACHE_VERSION' sw.js
 grep 'app.js?v=' index.html
 grep 'theme.css?v=' index.html
 
